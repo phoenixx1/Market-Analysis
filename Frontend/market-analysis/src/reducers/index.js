14e1d58acaf67c72ad6262b65a5e8a0419817339
phoenixx1 <xphoenix1010@gmail.com>
2021-04-11 16:14:11 +0530

    removed unwanted files

--- Frontend/market-analysis/src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import nameReducer from "./nameReducer";
-import newsReducer from "./newsReducer";
-
-export default combineReducers({ company: nameReducer, news: newsReducer });
--- Frontend/market-analysis/src/reducers/nameReducer.js
+++
@@ -1,8 +0,0 @@
-export default (state = [], action) => {
-  switch (action.type) {
-    case "FETCH_NAMES":
-      return [...state, action.payload];
-    default:
-      return state;
-  }
-};
